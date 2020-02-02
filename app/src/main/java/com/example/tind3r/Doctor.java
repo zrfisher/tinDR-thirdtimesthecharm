57d5df0b33857138d284ad898afbfb4edcfea882
Harshitha Padiyar <harshitha.g.padiyar@gmail.com>
2020-02-01 23:02:32 -0800

    html parsing + doctor class

--- app/src/main/java/com/example/tind3r/Doctor.java
+++ app/src/main/java/com/example/tind3r/Doctor.java
@@ -1,4 +1,45 @@
 package com.example.tind3r;
 
 public class Doctor {
+    String name;
+    String image_url;
+    String contact_url;
+    String phone;
+    String bio;
+
+    public Doctor(String name, String image_url,
+                  String contact_url, String phone,
+                  String bio)
+    {
+        this.name = name;
+        this.image_url = image_url;
+        this.contact_url = contact_url;
+        this.phone = phone;
+        this.bio = bio;
+    }
+
+    public String get_name()
+    {
+        return name;
+    }
+
+    public String get_image_url()
+    {
+        return image_url;
+    }
+
+    public String get_contact_url()
+    {
+        return contact_url;
+    }
+
+    public String get_phone()
+    {
+        return phone;
+    }
+
+    public String get_bio()
+    {
+        return bio;
+    }
 }
--- app/src/main/java/com/example/tind3r/FetchData.java
+++ app/src/main/java/com/example/tind3r/FetchData.java
@@ -1,23 +1,97 @@
 package com.example.tind3r;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.AsyncTask;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.jsoup.Connection;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class FetchData extends AsyncTask<Void, Void, Void> {
 
     String url;
     private ArrayList<Doctor> drs;
 
-
     public FetchData(String s){
         url = s;
     }
 
     @Override
-    protected Void doInBackground(Void... voids) {
+    protected Void doInBackground(Void... voids)
+    {
+        // get html from search results page
+        // parse to get list of profile links
+        Document resultsDoc;
+        String profile_link;
+        ArrayList<String> profileLinks = new ArrayList<String>;
 
-        //todo harshitha
+        try {
+            resultsDoc = Jsoup.connect(url).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+            resultsDoc = null;
+        }
+
+        if (resultsDoc != null) {
+            // <div class="result-row normal-result row"
+            Elements profiles = resultsDoc.select("result-row.normal-result.row");
+            for (Element profile : profiles) {
+                profile_link = profile.attr("data-profile-url");
+                profileLinks.add(profile_link);
+            }
+        }
+
+        // put this code in a loop, (for profileurl in list)
+        // iterate through urls from results page
+        // create a Doctor and add to ArrayList
+        Document doc;
+        for (String profile_url : profileLinks) {
+            try {
+                doc = Jsoup.connect(profile_url).get();
+            } catch (IOException e) {
+                e.printStackTrace();
+                doc = null;
+            }
+
+            if (doc != null) {
+                Element profileContainer = doc.select("#profileContainer").first();
+                String doctorName = profileContainer.attr("data-prof-name");
+                String image_url = profileContainer.attr("data-profile-image");
+
+                //currently gets link to profile since the email/contact url listed doesn't work
+                String contact_url = profileContainer.attr("data-profile-url");
+
+                String phone = profileContainer.attr("data-phone");
+
+                // bio is in a meta tag so extract this separately
+                String bio = "";
+                Elements metaTags = doc.getElementsByTag("meta");
+                for (Element metaTag : metaTags) {
+                    String content = metaTag.attr("content");
+                    String metaTagName = metaTag.attr("name");
+
+                    if ("description".equals(metaTagName)) {
+                        bio = content;
+                    }
+                }
+
+                Log.d("test", "test2");
+
+                Doctor d = new Doctor(doctorName, image_url, contact_url, phone, bio);
+                drs.add(d);
+            }
+        }
+
         return null;
     }
 
