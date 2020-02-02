74435bb60b533d2176c590a7ac331638837c5058
Harshitha Padiyar <harshitha.g.padiyar@gmail.com>
2020-02-02 05:19:17 -0800

    swipe?

--- app/src/main/java/com/example/tind3r/SwipeActivity.java
+++ app/src/main/java/com/example/tind3r/SwipeActivity.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v4.app.ActivityCompat;
@@ -20,10 +22,17 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.andtinder.model.CardModel;
+import com.andtinder.model.Orientations;
+import com.andtinder.view.CardContainer;
+import com.andtinder.view.SimpleCardStackAdapter;
 import com.squareup.picasso.Picasso;
 
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class SwipeActivity extends AppCompatActivity {
@@ -35,15 +44,74 @@ public class SwipeActivity extends AppCompatActivity {
     private int MY_PERMISSIONS_REQUEST_CALL_PHONE;
     int count = -1;
     Doctor d;
+    CardContainer mCardContainer;
+    SimpleCardStackAdapter adapter;
+    Drawable draw;
 
+    private class SetDrawable extends AsyncTask<Void, Void, Void> {
+        URL url;
+        Doctor d;
+        public SetDrawable(URL u, Doctor doc) {
+            url = u;
+            d = doc;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            Bitmap bmp = null;
+            try
+            {
+                bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+            } catch(IOException e)
+            {
+                e.printStackTrace();
+            }
+            draw = new BitmapDrawable(getResources(),bmp);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid){
+            CardModel c = new CardModel(d.get_name(), d.get_name(), draw);
+            c.setOnCardDimissedListener(new CardModel.OnCardDimissedListener() {
+                @Override
+                public void onLike() {
+                    Log.d("Swipeable Card", "I liked it");
+                }
+
+                @Override
+                public void onDislike() {
+                    Log.d("Swipeable Card", "I did not liked it");
+                }
+            });
+
+            adapter.add(c);
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swipe);
-
-        picture = findViewById(R.id.drPhoto);
+        mCardContainer = (CardContainer) findViewById(R.id.layoutview);
+        mCardContainer.setOrientation(Orientations.Orientation.Disordered);
         bio = findViewById(R.id.drBio);
-        name = findViewById(R.id.drName);
+        adapter = new SimpleCardStackAdapter(this);
+
+        for (Doctor doctor : doctors) {
+            URL url = null;
+            try {
+                url = new URL(doctor.get_image_url());
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+
+            SetDrawable sd = new SetDrawable(url, doctor);
+            sd.execute();
+
+
+        }
+
+        mCardContainer.setAdapter(adapter);
 
         callButton = findViewById(R.id.callButton);
         callButton.setOnClickListener(new View.OnClickListener() {
@@ -101,9 +169,7 @@ public class SwipeActivity extends AppCompatActivity {
     private void loadNextDr(){
         count++;
         d = doctors.get(count);
-        name.setText(d.get_name());
         bio.setText(d.get_bio());
-        Picasso.get().load(d.get_image_url()).into(picture);
     }
 
 
